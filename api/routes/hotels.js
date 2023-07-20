abd999c331f16f561e3d80bb1174cc58d071799b
omarfurkan <omarfurkan92@gmail.com>
2023-07-20 17:24:12 +0600

    filtered api by type and city

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import Hotel from '../models/Hotel.js';
-import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from '../controllers/hotelController.js';
+import { countByCity, countByType, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from '../controllers/hotelController.js';
 import { verifyAdmin } from '../utils/verifyToken.js';
 
 const router = express.Router();
@@ -12,9 +12,13 @@ router.put('/:id', verifyAdmin, updateHotel)
 //delete
 router.delete('/:id', verifyAdmin, deleteHotel)
 // get 
-router.get('/:id', getHotel)
+router.get('/find/:id', getHotel)
 // get all
 router.get('/', getHotels)
+
+
+router.get('/countByCity', countByCity)
+router.get('/countByType', countByType)
 
 
 
