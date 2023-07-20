b420f22bdd38bad93702a61c4bc6bf03cd03214f
omarfurkan <omarfurkan92@gmail.com>
2023-07-20 12:22:28 +0600

    hotel and user route is jwt done

--- api/routes/users.js
+++ api/routes/users.js
@@ -4,25 +4,25 @@ import { verifyAdmin, verifyToken, verifyUser } from '../utils/verifyToken.js';
 const router = express.Router();
 
 
-router.get('/checkauthetication', verifyToken, (req, res, next) => {
-    res.send('hello user, you are logged in')
-})
+// router.get('/checkauthetication', verifyToken, (req, res, next) => {
+//     res.send('hello user, you are logged in')
+// })
 
-router.get('/checkuser/:id', verifyUser, (req, res, next) => {
-    res.send('hello user, you are logged in and you can delete your account')
-})
-router.get('/checkadmin/:id', verifyAdmin, (req, res, next) => {
-    res.send('hello admin, you are logged in and you can delete all accounts')
-})
+// router.get('/checkuser/:id', verifyUser, (req, res, next) => {
+//     res.send('hello user, you are logged in and you can delete your account')
+// })
+// router.get('/checkadmin/:id', verifyAdmin, (req, res, next) => {
+//     res.send('hello admin, you are logged in and you can delete all accounts')
+// })
 
 //update
-router.put('/:id', updateUser)
+router.put('/:id', verifyUser, updateUser)
 //delete
-router.delete("/:id", deleteUser)
+router.delete("/:id", verifyUser, deleteUser)
 //get
-router.get("/:id", getUser)
+router.get("/:id", verifyUser, getUser)
 //gell all
-router.get("/", getUsers)
+router.get("/", verifyAdmin, getUsers)
 
 export default router;
 
--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -16,7 +16,7 @@ export const verifyToken = (req, res, next) => {
 
 
 export const verifyUser = (req, res, next) => {
-    verifyToken(req, res, () => {
+    verifyToken(req, res, next, () => {
         if (req.user.id === req.params.id || req.user.isAdmin) {
             next()
         }
@@ -28,7 +28,7 @@ export const verifyUser = (req, res, next) => {
 
 
 export const verifyAdmin = (req, res, next) => {
-    verifyToken(req, res, () => {
+    verifyToken(req, res, next, () => {
         if (req.user.isAdmin) {
             next()
         }
