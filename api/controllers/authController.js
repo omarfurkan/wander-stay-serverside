042cd3eaaf3ae9060fe5dade6d47c863b2118614
omarfurkan <omarfurkan92@gmail.com>
2023-07-23 14:18:42 +0600

    slight changes in controlllers

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,8 +1,7 @@
-import User from "../models/User.js"
-import bcrypt from 'bcryptjs'
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
 import { createError } from "../utils/error.js";
 import jwt from "jsonwebtoken";
-
 
 export const register = async (req, res, next) => {
     try {
@@ -13,32 +12,38 @@ export const register = async (req, res, next) => {
             ...req.body,
             password: hash,
         });
-        await newUser.save()
-        res.status(200).send('User has been created')
+
+        await newUser.save();
+        res.status(200).send("User has been created.");
     } catch (err) {
-        next(err)
+        next(err);
     }
-}
+};
 export const login = async (req, res, next) => {
     try {
         const user = await User.findOne({ username: req.body.username });
-        console.log(user)
         if (!user) return next(createError(404, "User not found!"));
 
-        const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
+        const isPasswordCorrect = await bcrypt.compare(
+            req.body.password,
+            user.password
+        );
         if (!isPasswordCorrect)
             return next(createError(400, "Wrong password or username!"));
 
-        const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT)
+        const token = jwt.sign(
+            { id: user._id, isAdmin: user.isAdmin },
+            process.env.JWT
+        );
 
         const { password, isAdmin, ...otherDetails } = user._doc;
         res
             .cookie("access_token", token, {
-                httpOnly: true
+                httpOnly: true,
             })
             .status(200)
-            .json({ ...otherDetails });
+            .json({ details: { ...otherDetails }, isAdmin });
     } catch (err) {
-        next(err)
+        next(err);
     }
-}
+};
